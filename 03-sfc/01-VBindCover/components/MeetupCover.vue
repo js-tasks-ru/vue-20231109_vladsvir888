9c169247595b0fbb72350233b31a3c7ec27ccda2
vsvirydovich <vladsvir888@gmail.com>
2023-11-26 15:05:59 +0300

    add VBindCover

--- 03-sfc/01-VBindCover/components/MeetupCover.vue
+++ 03-sfc/01-VBindCover/components/MeetupCover.vue
@@ -1,12 +1,16 @@
 <template>
   <div class="meetup-cover">
-    <h1 class="meetup-cover__title">Title</h1>
+    <h1 class="meetup-cover__title">{{ title }}</h1>
   </div>
 </template>
 
 <script>
 export default {
   name: 'MeetupCover',
+  props: {
+    title: String,
+    image: String,
+  },
 };
 </script>
 
@@ -15,8 +19,7 @@ export default {
   background-size: cover;
   background-position: center;
   background-image: linear-gradient(0deg, rgba(0, 0, 0, 0.4), rgba(0, 0, 0, 0.4)),
-    url('https://course-vue.javascript.ru/api/images/2');
-  /* background-image: linear-gradient(0deg, rgba(0, 0, 0, 0.4), rgba(0, 0, 0, 0.4)), var(--default-cover); */
+    v-bind(image ? `url('${image}') `: 'var(--default-cover)');
   display: flex;
   flex-direction: column;
   align-items: center;
