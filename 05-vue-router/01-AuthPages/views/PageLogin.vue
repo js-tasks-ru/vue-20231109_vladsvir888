a51b9b80e96cacadbe3ea6cd3a51702753b6c8bb
vsvirydovich <vladsvir888@gmail.com>
2023-12-03 20:11:32 +0300

    add AuthPages

--- 05-vue-router/01-AuthPages/views/PageLogin.vue
+++ 05-vue-router/01-AuthPages/views/PageLogin.vue
@@ -16,7 +16,9 @@
         <div class="form__buttons">
           <button type="submit" class="button button_primary button_block">Войти</button>
         </div>
-        <div class="form__append">Нет аккаунта? <a href="/register" class="link">Зарегистрируйтесь</a></div>
+        <div class="form__append">
+          Нет аккаунта? <RouterLink :to="{ name: 'register' }" class="link">Зарегистрируйтесь</RouterLink>
+        </div>
       </form>
     </UiContainer>
   </div>
@@ -36,7 +38,11 @@ export default {
 
   methods: {
     handleSubmit() {
-      // Требуется обработать сабмит формы
+      if (this.$route.query.from) {
+        return this.$router.push(this.$route.query.from);
+      }
+
+      this.$router.push({ name: 'index' });
     },
   },
 };
