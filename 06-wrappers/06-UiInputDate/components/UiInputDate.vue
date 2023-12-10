13d59c90ffb1e297f0b38b4c6aabdb701de9ddbf
vsvirydovich <vladsvir888@gmail.com>
2023-12-10 14:32:43 +0300

    add UiInputDate

--- 06-wrappers/06-UiInputDate/components/UiInputDate.vue
+++ 06-wrappers/06-UiInputDate/components/UiInputDate.vue
@@ -1,13 +1,69 @@
 <template>
-  <UiInput />
+  <UiInput ref="inputDate" :type="type" v-model="model">
+    <template v-for="slotName in Object.keys($slots)" #[slotName]>
+      <slot :name="slotName" />
+    </template>
+  </UiInput>
 </template>
 
 <script>
 import UiInput from './UiInput.vue';
 
+function padTo2Digits(num) {
+  return num.toString().padStart(2, '0');
+}
+
 export default {
   name: 'UiInputDate',
-
   components: { UiInput },
+  props: {
+    type: {
+      type: String,
+      validator: (value) => ['date', 'time', 'datetime-local'].includes(value),
+      default: 'date',
+    },
+    modelValue: {
+      type: Number,
+    },
+    step: {
+      type: Number,
+    },
+  },
+  emits: ['update:modelValue'],
+  computed: {
+    model: {
+      get() {
+        // date - yyyy-mm-dd
+        // time - hh:mm
+        // datetime-local - yyyy-mm-ddThh:mm
+        if (!this.modelValue) return null;
+
+        const date = new Date(this.modelValue);
+        const year = date.getUTCFullYear();
+        const month = padTo2Digits(date.getUTCMonth() + 1);
+        const day = padTo2Digits(date.getUTCDate());
+        const hours = padTo2Digits(date.getUTCHours());
+        const minutes = padTo2Digits(date.getUTCMinutes());
+        let value;
+
+        switch (this.type) {
+          case 'time':
+            value = `${hours}:${minutes}`;
+            break;
+          case 'datetime-local':
+            value = `${year}-${month}-${day}T${hours}:${minutes}`;
+            break;
+          default:
+            value = `${year}-${month}-${day}`;
+            break;
+        }
+
+        return value;
+      },
+      set() {
+        this.$emit('update:modelValue', this.$refs.inputDate.$refs.input.valueAsNumber);
+      },
+    },
+  },
 };
 </script>
