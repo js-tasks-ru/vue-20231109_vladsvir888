48ff1889cbc09373f67c96ca0180fe938982ba96
vsvirydovich <vladsvir888@gmail.com>
2024-01-02 21:32:38 +0300

    add ProgressPlugin

--- 13-reusability/01-ProgressPlugin/main.js
+++ 13-reusability/01-ProgressPlugin/main.js
@@ -3,6 +3,9 @@ import App from './App.vue';
 import { router } from './router/index.js';
 import { createProgress } from './plugins/progress/index.js';
 
-const progress = createProgress({ container: '#progress', router });
+const progress = createProgress({ router });
 
-createApp(App).use(router).mount('#app');
+createApp(App)
+  .use(router)
+  .use(progress)
+  .mount('#app');
